9740856373a37fbddbfd4e2d030296ca4b1c9664
zjutsxj <zjutsxj@qq.com>
2020-05-30 22:30:44 +0800

    区分发送验证码，发送密码

--- src/Sms.php
+++ src/Sms.php
@@ -35,9 +35,9 @@ class Sms
         $this->config = array_merge($this->config, $config);
     }
 
-    public function sendSms($phone, $code)
+    // 发送验证码
+    public function sendCode($phone, $code)
     {
-
         $config = $this->config;
         $keys = ['access_key', 'access_secret', 'sign_name', 'template_code'];
         foreach ($keys as $key) {
@@ -85,8 +85,53 @@ class Sms
         return false;
     }
 
-    public function getError()
+    // 发送密码
+    public function sendPassword($phone, $password)
     {
-        return $this->error;
+        $config = $this->config;
+        $keys = ['access_key', 'access_secret', 'sign_name', 'template_code'];
+        foreach ($keys as $key) {
+            if (empty($config[$key])) {
+                $this->error = $key . ' 不存在!';
+                return false;
+            }
+        }
+
+        $params = array();
+
+        // fixme 必填: 短信接收号码
+        $params["PhoneNumbers"] = $phone;
+
+        // fixme 必填: 短信签名，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
+        $params["SignName"] = $config['sign_name'];
+
+        // fixme 必填: 短信模板Code，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
+        $params["TemplateCode"] = $config['template_code'];
+
+        // fixme 可选: 设置模板参数, 假如模板中存在变量需要替换则为必填项
+        $params['TemplateParam'] = array(
+            "password" => $password,
+        );
+
+        // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
+        if (!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+            $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+        }
+
+        $params = array_merge($params, array(
+            "RegionId" => "cn-hangzhou",
+            "Action" => "SendSms",
+            "Version" => "2017-05-25",
+        ));
+
+        $helper = new SignatureHelper;
+        // 此处可能会抛出异常，注意catch
+        try {
+            $content = $helper->request($config['access_key'], $config['access_secret'], $config['host'], $params, $config['security']);
+            return $content;
+        } catch (\Exception $e) {
+            $this->error = $e->getMessage();
+        }
+        return false;
     }
 }
